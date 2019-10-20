eb0fffaa6e19770fb46893a263ac48f8a1c5fe15
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-10-20 09:56:28 +0200

    Update zadanie1.cmd

--- zadanie1.cmd
+++ zadanie1.cmd
@@ -1,1 +1,6 @@
+// 1. Zapoznać się z możliwościami konﬁguracji okna Wiersza polecenia.
+// (a) Zmień tytuł okna wiersza polecenia na Systemy operacyjne - laboratorium. 
+// (b) Zmień kolor tla wiersza polecenia na niebieski, a kolor czcionki na żółty.
+
+@echo off
 title "Systemy operacyjne - laboratorium" & color 16
