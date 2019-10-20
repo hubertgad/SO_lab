c7c9aa20e110ea49d8c71ac25773597070440582
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-10-20 19:15:09 +0200

    Create zadanie32.cmd

---
+++ zadanie32.cmd
@@ -0,0 +1,5 @@
+@echo off
+
+REM // 32. Napisać skrypt, który będzie podawał status wyjścia aplikacji uruchomionej przez skrypt. Skrypt ma zapytać się użytkownika o nazwę aplikacji, którą ma uruchomić.
+
+
