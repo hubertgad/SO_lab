599e069b113c735ea46ad5a53ac7917dd4dfd4f5
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-10-23 16:37:31 +0200

    Create zadanie2.cmd

---
+++ lab2/zadanie2.cmd
@@ -0,0 +1,8 @@
+@echo off
+REM 2. Utwórz następujące konta użytkowników przynależące do różnych grup:
+REM • Student1 – Użytkownicy zaawansowani,
+REM • Student2 – Użytkownicy,
+REM • Student3 – Goście,
+REM • Student4 – Administratorzy,
+REM • Student5 – Studenci.
+
