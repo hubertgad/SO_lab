ce58e72864bf5146a7f3e47a3286a03ee6a8e077
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-10-24 13:09:30 +0200

    Create zadanie7.cmd

---
+++ lab2/zadanie7.cmd
@@ -0,0 +1,4 @@
+REM 7. Jako administrator utwórz konto o nazwie Jan i przypisz mu folder macierzysty w następującej lokalizacji: C:\Foldery_macierzyste\Jan.
+
+mkdir C:\Foldery_macierzyste\Jan
+net user Jan * /HOMEDIR:C:\Foldery_macierzyste\Jan /add
