036241501a47f7ef85b17c146244c24e7f796870
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-10-24 13:06:15 +0200

    Update zadanie5.cmd

--- lab2/zadanie5.cmd
+++ lab2/zadanie5.cmd
@@ -4,5 +4,9 @@ REM 5. Z linii poleceń otwórz program Zasady zabezpieczeń lokalnych:
 REM (a) Przejdź do folderu Ustawienia zabezpieczeń\Zasady konta\Zasady haseł
 REM (b) Przejrzyj wszystkie opcje i zapoznaj się z kartą Wyjaśnienie.
 REM (c) Włączyć opcje dotyczące komplikacji hasła, długość min 8 znaków oraz ważność hasła ustawić na 14 dni. Sprawdzić czy można podać hasło nie spełniające powyższych wymagań.
+REM 6. Otwórz ponownie program Zasady zabezpieczeń lokalnych:
+REM (a) Przejdź do folderu Ustawienia zabezpieczeń\Zasady konta\Zasady blokady konta
+REM (b) Przejrzyj wszystkie opcje i zapoznaj się z kartą Wyjaśnienie.
+REM (c) Włączyć opcje blokady konta tak, aby było blokowane na jedną minutę po wprowadzeniu trzech błędnych haseł. Dokonać sprawdzenia włączonych mechanizmów.
 
 secpol.msc
