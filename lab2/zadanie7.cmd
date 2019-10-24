62513b2fd8967f9480c3d57c6825b02b979332b2
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-10-24 13:10:08 +0200

    Update zadanie7.cmd

--- lab2/zadanie7.cmd
+++ lab2/zadanie7.cmd
@@ -1,5 +1,5 @@
-REM 7. Jako administrator utwórz konto o nazwie Jan i przypisz mu folder macierzysty w następującej lokalizacji: 
-REM C:\Foldery_macierzyste\Jan.
+@echo off REM 7. Jako administrator utwórz konto o nazwie Jan i przypisz mu folder macierzysty w następującej lokalizacji: 
+REM C:\Foldery_macierzyste\Jan
 
 mkdir C:\Foldery_macierzyste\Jan
 net user Jan * /HOMEDIR:C:\Foldery_macierzyste\Jan /add
