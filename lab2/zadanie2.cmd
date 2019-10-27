691965d7f129bacdf638b35affffe0123f2d8173
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-10-27 09:34:14 +0100

    Update zadanie2.cmd

--- lab2/zadanie2.cmd
+++ lab2/zadanie2.cmd
@@ -6,13 +6,14 @@ REM • Student3 – Goście,
 REM • Student4 – Administratorzy,
 REM • Student5 – Studenci.
 
-net user Student1 * /ADD 
+net user Student1 fred_ąć /ADD 
 net localgroup "Użytkownicy zaawansowani" Student1 /add
-net user Student2 * /ADD 
+net user Student2 fred_ąć /ADD 
 net localgroup "Użytkownicy" Student2 /add
-net user Student3 * /ADD 
+net user Student3 fred_ąć /ADD 
 net localgroup "Goście" Student3 /add
-net user Student4 * /ADD 
+net user Student4 fred_ąć /ADD 
 net localgroup "Administratorzy" Student4 /add
-net user Student5 * /ADD 
+net user Student5 fred_ąć /ADD
+net localgroup "Studenci" /add
 net localgroup "Studenci" Student5 /add
