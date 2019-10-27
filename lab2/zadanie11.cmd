2ebf2b1e351eb467a0b564a3250f0818fb183080
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-10-27 10:41:13 +0100

    Create zadanie11.cmd

---
+++ lab2/zadanie11.cmd
@@ -0,0 +1,4 @@
+REM 11. Napisać polecenie, które spowoduje dodanie do systemu użytkownika o podanej nazwie, 
+REM który będzie mógł się zalogować jedynie w dni robocze, w godzinach od 8 do 15.
+
+net user Jan /times:Pn-Pt,8-15
