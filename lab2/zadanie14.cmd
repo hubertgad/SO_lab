f7b5b4ed8bff8730db4bdcecac6e155c9bd2a75d
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-10-27 10:46:15 +0100

    Create zadanie14.cmd

---
+++ lab2/zadanie14.cmd
@@ -0,0 +1,10 @@
+REM 14. Napisać skrypt, który pobierze listę użytkowników z pliku tekstowego, a następnie utworzy dla nich konta. Nazwa pliku ma zostać podana jako parametr wejściowy do skryptu. W przypadku
+braku parametru wejściowego skrypt powinien wyświetlić odpowiedni komunikat. Skrypt ma także sprawdzać czy plik, którego nazwę podano jako parametr wejśćiowy istnieje w podanej lokalizacji. Zawartość pliku z danymi powinna wyglądać następująco:
+1,Jan Kowalski,kowalj
+2,Jozef Nowak,nowakj
+.
+.
+.
+10,Funky Koval,funky
+11,Brenda,brenda
+Jako separatora pól zastosować przecinek.
