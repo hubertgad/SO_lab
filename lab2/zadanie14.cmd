75ccaa087561b4a393d9950a6eb86a7c44ad7762
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-11-03 14:39:29 +0100

    Update zadanie14.cmd

--- lab2/zadanie14.cmd
+++ lab2/zadanie14.cmd
@@ -1,10 +1,13 @@
-REM 14. Napisać skrypt, który pobierze listę użytkowników z pliku tekstowego, a następnie utworzy dla nich konta. Nazwa pliku ma zostać podana jako parametr wejściowy do skryptu. W przypadku
-braku parametru wejściowego skrypt powinien wyświetlić odpowiedni komunikat. Skrypt ma także sprawdzać czy plik, którego nazwę podano jako parametr wejśćiowy istnieje w podanej lokalizacji. Zawartość pliku z danymi powinna wyglądać następująco:
-1,Jan Kowalski,kowalj
-2,Jozef Nowak,nowakj
-.
-.
-.
-10,Funky Koval,funky
-11,Brenda,brenda
-Jako separatora pól zastosować przecinek.
+REM 14. Napisać skrypt, który pobierze listę użytkowników z pliku tekstowego, a następnie utworzy dla nich konta. 
+REM Nazwa pliku ma zostać podana jako parametr wejściowy do skryptu. 
+REM W przypadku braku parametru wejściowego skrypt powinien wyświetlić odpowiedni komunikat. 
+REM Skrypt ma także sprawdzać czy plik, którego nazwę podano jako parametr wejśćiowy istnieje w podanej lokalizacji. 
+REM Zawartość pliku z danymi powinna wyglądać następująco:
+REM 1,Jan Kowalski,kowalj
+REM 2,Jozef Nowak,nowakj
+REM .
+REM .
+REM .
+REM 10,Funky Koval,funky
+REM 11,Brenda,brenda
+REM Jako separatora pól zastosować przecinek.
