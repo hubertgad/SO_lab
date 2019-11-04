8fedbc12d02294caf107a911508bc68ce61e0196
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-11-04 16:51:30 +0100

    Create zadanie25.cmd

---
+++ lab2/zadanie25.cmd
@@ -0,0 +1,8 @@
+@echo off
+REM 25. Zapoznaj się z poleceniem query, a w szczególności z user i session.
+REM (a) Sprawdź ilu obecnie użytkowników jest zalogowanych w systemie?
+REM (b) Sprawdź jaki numer identyfikacyjny (ID) ma sesja administratora i użytkownika Student1, godzinę logowania, nazwę sesji oraz stan.
+REM (c) Porównaj wyniki z tym co prezentuje Menedżer zadań w trybie graficznym. 
+REM Podaj także jakim poleceniem można uruchomić Menedżera zadań?
+
+QUERY user
