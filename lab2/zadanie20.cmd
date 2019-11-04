963d668a5f3a004d18bddcd089c6f844d2efe313
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-11-04 19:50:38 +0100

    Update zadanie20.cmd

--- lab2/zadanie20.cmd
+++ lab2/zadanie20.cmd
@@ -2,6 +2,10 @@
 REM 20. Zapoznać się z programem diskpart. Za jego pomocą wyświetlić
 REM (a) szczegółowe informacje o dostępnych dyskach, wolumenach i partycjach
 REM (b) nazwy partycji oraz ich rozmiary
-diskpart list disk
-diskpart list volume
-diskpart list partition
+
+diskpart 
+list disk
+list volume
+
+select disk 0
+list partition
