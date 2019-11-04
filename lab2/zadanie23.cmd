c23030466d0f729214855b0d2d1ede70c4e2cb7f
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-11-04 16:40:14 +0100

    Create zadanie23.cmd

---
+++ lab2/zadanie23.cmd
@@ -0,0 +1,6 @@
+REM 23. Sprawdzić jakie są obecnie ustawione limity przydziałów: fsutil quota query C:.
+REM Następnie ustawić użytkownikowi Student1 limity na 60 MB (poziom ostrzeżeń, w bajtach: 62914560 = 60·220) 
+REM i 100 MB (limit końcowy, w bajtach: 104857600 = 100·220).
+REM Ponownie sprawdzić ustawienia linitu przydziałów.
+REM Następnie zmienić limity na 80 MB (83886080 B, poziom ostrzeżeń) i 120MB (125829120 B, limit końcowy).
+
