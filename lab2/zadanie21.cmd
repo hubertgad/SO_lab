e30c5afb929b566883af9d2c6f84b9aac538a333
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-11-04 16:36:13 +0100

    Create zadanie21.cmd

---
+++ lab2/zadanie21.cmd
@@ -0,0 +1,7 @@
+@echo off
+REM 21. Zapoznać się z poleceniem fsutil.
+REM Zwrócić uwagę na właściwą kolumnę.
+
+REM Performs tasks that are related to file allocation table (FAT) and NTFS file systems, 
+REM such as managing reparse points, managing sparse files, or dismounting a volume. 
+REM If it is used without parameters, Fsutil displays a list of supported subcommands.
