928ff4bf32c36d9d89b199ed7af761cd54e9a020
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-11-04 16:14:19 +0100

    Update and rename Zadania.txt to zadanie19.cmd

---
+++ lab2/zadanie19.cmd
@@ -0,0 +1,5 @@
+@echo off
+REM 19. Za poznaj się z poleceniem takeown.
+
+REM Enables an administrator to recover access to a file that previously was denied, by making the administrator the owner of the file.
+REM takeown [/s <Computer> [/u [<Domain>\]<User name> [/p [<Password>]]]] /f <File name> [/a] [/r [/d {Y|N}]]
