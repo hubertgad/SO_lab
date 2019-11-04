1f09203caa20e7ab89556777c4d8a955a52b3baa
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-11-04 16:41:23 +0100

    Create zadanie24.cmd

---
+++ lab2/zadanie24.cmd
@@ -0,0 +1,5 @@
+@echo off
+REM 24. Zaloguj się na konto Student1. Uruchom notatnik i napisz w nim dowolny tekst bez zapisywania go. Notatnik pozostaw uruchomiony.
+REM • Podaj jaki masz SID2
+REM .
+REM • Przeloguj się na konto administratora (bez wylogowywania).
