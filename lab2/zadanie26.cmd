a3da3476cc04a6bbe926a2d67d44fe277706ad3a
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-11-04 16:51:49 +0100

    Create zadanie26.cmd

---
+++ lab2/zadanie26.cmd
@@ -0,0 +1,48 @@
+@echo off
+REM 26. Zapoznaj sie z poleceniem query process oraz uruchom notatnik.
+REM (a) Wyświetl osobno procesy dla administratora i użytkownika Student1.
+REM (b) Wyświetl procesy, które są obecnie używane w sesji o nazwie console.
+REM (c) Wyświetl procesy, które są skojarzone z notatnikiem. Zwróć uwagę na PID procesu i z jakim użytkownikiem i sesją są skojarzone.
+REM (d) Zadanie domowe: wyjaśnij pojęcie PID.
+REM (e) Porównaj wyniki z tym co prezentuje Menedżer zadań w trybie graficznym.
+
+@echo off
+REM 27. Zapoznaj się z poleceniem tasklist. Wykonaj podpunkty (a)-(c) z zadania 26. 
+REM  Wskaż, czy są różnice w wynikach, czy jest ich tyle samo, jak są wyświetlane? 
+REM Porównaj wyniki z uzyskanymi w poprzednich zadaniach.
+
+@echo off
+REM 28. Jako administrator z poziomu Menedżera zadań zakończ pracę notatnika.
+
+@echo off
+REM 29. Będąc zalogowanym na koncie administratora i korzystając z polecenia query, tasklist, tskill
+REM  lub taskkill zakończ działanie notatnika dla konta Student1 (zwróć uwagę, że notatnik pozostał uruchomiony z niezapisanym tekstem).
+
+@echo off
+REM 30. Korzystając z programu Podgląd zdarzeń określ przybliżony czas ostatniego uruchomienia komputera.
+
+@echo off
+REM 31. Zapoznaj się z poleceniem systeminfo i porównaj czasy uruchomienia komputera.
+
+@echo off
+REM 32. Dokonaj inspekcji zdarzeń logowania na kontach. Do dziennika bezpieczeństwa należy zapisywaćzdarzenia Sukces jak i Niepowodzenie.
+REM Wskazówka: użyj polecenia secpol.msc.
+
+@echo off
+REM 33. Otwórz plik zawierający dziennik bezpieczeństwa i określ informacje dotyczące pierwszego zdarzenia.
+
+@echo off
+REM 34. Zapoznaj się z poleceniem gpresult.
+
+@echo off
+REM 35. Wyświetl raport zawierający zasady wynikowe dla użytkownika Student1.
+REM 2
+REM https://msdn.microsoft.com/en-us/library/windows/desktop/aa379649(v=vs.85).aspx,
+REM http://technet.microsoft.com/en-us/library/cc778824(WS.10).aspx
+REM 3
+
+@echo off
+REM 36. Wyświetl raport zawierający zasady wynikowe dla komputera.
+
+@echo off
+REM 37. Wygeneruj raport w postaci pliku c:\raport\student1.html
