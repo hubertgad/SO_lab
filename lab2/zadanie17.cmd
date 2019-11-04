1833ec4d52e8c5b0d95ddcc88a1527f55b9f112e
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-11-04 15:02:51 +0100

    Create zadanie17.cmd

---
+++ lab2/zadanie17.cmd
@@ -0,0 +1,7 @@
+@echo off 
+REM 17. Zalogować się jako administrator. Utworzyć folder C:\dane_admina. 
+REM Nadaj użytkownikowi admin prawo Pełna kontrola do tego folderu i utworzyć wewnątrz niego plik plik_admina.txt o dowolnej treści.
+
+MKDIR "C:\dane_admina"
+ICACLS "C:\dane_admina" /grant "admin:F"
+dowolna_treść > C:\dane_admina\plik_admina.txt
