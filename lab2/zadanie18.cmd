02db156ea95a5561b7c730770a0f1ed4650cada0
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-11-04 15:05:08 +0100

    Create zadanie18.cmd

---
+++ lab2/zadanie18.cmd
@@ -0,0 +1,5 @@
+@echo off
+REM 18. Zalogować się odpowiednio jako Student1, Student2 oraz Student3 i wykonać na pliku plik_admina.txt operacje:
+REM otwarcia, zmiany zawartości, zapisu, usunięcia. 
+REM Wykonanie których operacji jest możliwe?
+
