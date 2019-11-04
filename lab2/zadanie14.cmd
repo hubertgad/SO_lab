ccda79117e6c678863dc06d5b960e942f8e470a6
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-11-04 08:18:05 +0100

    Update zadanie14.cmd

--- lab2/zadanie14.cmd
+++ lab2/zadanie14.cmd
@@ -1,3 +1,4 @@
+@echo off
 REM 14. Napisać skrypt, który pobierze listę użytkowników z pliku tekstowego, a następnie utworzy dla nich konta. 
 REM Nazwa pliku ma zostać podana jako parametr wejściowy do skryptu. 
 REM W przypadku braku parametru wejściowego skrypt powinien wyświetlić odpowiedni komunikat. 
@@ -11,3 +12,13 @@ REM .
 REM 10,Funky Koval,funky
 REM 11,Brenda,brenda
 REM Jako separatora pól zastosować przecinek.
+
+IF [%1] EQU [] (
+ECHO Nie znaleziono parametru...
+GOTO :EOF )
+IF NOT EXIST %1.txt (
+ECHO Nie znaleziono pliku...
+GOTO :EOF )
+FOR /F "delims=, tokens=2-3" %%G in (%1.txt) DO (
+NET USET "%%G" /ADD )
+pause
