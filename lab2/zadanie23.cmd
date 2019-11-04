fc626e8ce57e3028bb877efcbff130853aef0745
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-11-04 20:40:08 +0100

    Update zadanie23.cmd

--- lab2/zadanie23.cmd
+++ lab2/zadanie23.cmd
@@ -5,3 +5,7 @@ REM i 100 MB (limit końcowy, w bajtach: 104857600 = 100·220).
 REM Ponownie sprawdzić ustawienia linitu przydziałów.
 REM Następnie zmienić limity na 80 MB (83886080 B, poziom ostrzeżeń) i 120MB (125829120 B, limit końcowy).
 
+fsutil quota query C:
+fsutil quota modify 62914560 104857600 Student1
+fsutil quota query C:
+fsutil quota modify 83886080 125829120 Student1
