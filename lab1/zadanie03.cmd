ec0fd5bd6b3242c2197c0f59e37890ae0c8b0801
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-11-04 20:48:16 +0100

    Create zadanie03.cmd

---
+++ lab1/zadanie03.cmd
@@ -0,0 +1,39 @@
+REM 3. Wyswietl godzine i date bez monitu o zmiane daty.
+
+REM 4. Sporzadz raport stanu dysku i umiesc go w pliku raport_dysk.
+
+REM 5. Napisz instrukcje, która spowoduje wyswietlenie na ekranie zawartosci pliku raport_dysk.
+
+REM 6. Napisac instrukcje, która wyswietli wszystkie pliki z katalogu głównego na dysku c.
+
+REM 7. Napisac instrukcje, która wyswietli wszystkie katalogi w katalogu głównym na dysku c.
+
+REM 8. Wyswietlic wszystkie pliki z katalogu system32 w kolejnosci alfabetycznej według rozszerzenia.
+
+REM 9. Napisac polecenie, które skopiuje wszystkie pliki z katalogu nadrzednego wzgledem biezacego do katalogu biezacego. Kopiowanie powinno odbyc sie z weryfikacja danych.
+
+REM 10. Utworzyc katalogi KAT1 i KAT2. Do katalogu Kat2 skopiowac pliki z katalogu system32, których druga litera nazwy jest d.
+
+REM 11. Napisac instrukcje, która spowoduje usuniecie pustego katalogu o nazwie KAT1.
+
+REM 12. Napisac instrukcje, która spowoduje usuniecie niepustego katalogu o nazwie KAT2.
+
+REM 13. Utworzyc plik o nazwie dane.h i nadac mu atrybut ukryty. Sprawdzic poleceniem dir, czy jest widoczny.
+
+REM 14. Napisac polecenie, które utworzy plik o nazwie dane.txt oraz umozliwi wpisanie 
+REM do niego ciagu znaków z poziomu wiersza polecenia. UWAGA!. Wykorzystac polecenie copy. 
+REM Koniec wpisywania znaków do pliku zasygnalizowac sekwencja ctrl+Z.
+
+REM 15. Zmienic znak zachety na biezaca godzine zakonczona znakiem wiekszosci.
+
+REM 16. Zapoznac sie z poleceniami: if, for, call, goto.
+
+REM 17. Utworzyc plik wsadowy, który usunie wszystkie pliki o rozszerzeniu .bak i .tmp z katalogu
+REM podanego jako parametr. Skrypt powinien sprawdzac czy podano parametr. 
+REM Jesli go nie podano, to na ekranie powinna zostac wydrukowana poprawna składnia uzycia polecenia.
+
+REM 18. Zmodyfikowac skrypt z zadania poprzedniego tak, aby maski plików do usuniecia były podawane z linii polecen. 
+REM Rozwazyc mozliwosc podania wiecej jak jednego parametru.
+
+REM 19. Napisac program wsadowy, który spowoduje wyswietlenie na ekranie nazwy zalogowanego uzytkownika,
+REM litere dysku oraz nazwe katalogu macierzystego oraz informacje o procesorze znajdujacym sie w systemie.
