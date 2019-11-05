12b47b8c34b711c33c2b9c06865aaa2c29e126f1
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-11-05 14:27:48 +0100

    Create zadanie31.cmd

---
+++ lab2/zadanie31.cmd
@@ -0,0 +1,6 @@
+@echo off
+REM 31. Zapoznaj się z poleceniem systeminfo i porównaj czasy uruchomienia komputera.
+
+:: Wyświetla szczegółowe informacje o komputerze oraz OS:
+:: konfiguracja OS, informacje o bezpieczeństwie, ID produktu,
+:: konfiguracja sprzętu (RAM, dysk, sieć).
