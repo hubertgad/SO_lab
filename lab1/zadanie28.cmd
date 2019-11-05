941bd5584f5602c0c64b3515a42a34593f87c674
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-11-05 11:41:01 +0100

    Update zadanie28.cmd

--- lab1/zadanie28.cmd
+++ lab1/zadanie28.cmd
@@ -1,7 +1,5 @@
 @echo off
-
-REM // 28. Zaplanować automatyczne wyłączenie oraz ponowne uruchomienie komputera.
+REM 28. Zaplanować automatyczne wyłączenie oraz ponowne uruchomienie komputera.
 
 schtasks /create /tn Shut-Down /sc ONCE /st 15:00 /tr "shutdown -s"
 schtasks /create /tn Shut-Down /sc ONCE /st 16:00 /tr "shutdown -r"
-pause
