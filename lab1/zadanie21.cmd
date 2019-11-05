28d1bccd2d883a068c41b459889143a4b8814517
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-11-05 11:38:09 +0100

    Update zadanie21.cmd

--- lab1/zadanie21.cmd
+++ lab1/zadanie21.cmd
@@ -1,6 +1,4 @@
 @echo off
-
-REM // 21. Napisać skrypt wypisujący na ekranie liczby całkowite z danego przedziału podanego z lini poleceń.
+REM 21. Napisać skrypt wypisujący na ekranie liczby całkowite z danego przedziału podanego z lini poleceń.
 
 FOR /L %%G IN (%1, 1, %2) DO (echo %G)
-pause
