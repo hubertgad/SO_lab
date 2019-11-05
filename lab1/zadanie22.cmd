9b8e2ddb094fe9cd323d6515a8a95c2c84f44e43
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-11-05 11:39:07 +0100

    Update zadanie22.cmd

--- lab1/zadanie22.cmd
+++ lab1/zadanie22.cmd
@@ -1,6 +1,5 @@
 @echo off
-
-REM //22. Napisać skrypt który zamieni rozszerzenia wszystkich plików *.txt na *.tx_. Rozszerzyć funkcjonalność skryptu o podawanie rozszerzeń jako parametry skryptu.
+REM 22. Napisać skrypt który zamieni rozszerzenia wszystkich plików *.txt na *.tx_. Rozszerzyć funkcjonalność skryptu o podawanie rozszerzeń jako parametry skryptu.
 
 REM RENAME *.txt *tx_ - wersja pierwsza
 RENAME *.%1 *.%2
