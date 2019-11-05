89b7e8b6057e5a70ae4d96aa59177ad333419063
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-11-05 11:34:14 +0100

    Update zadanie01.cmd

--- lab1/zadanie01.cmd
+++ lab1/zadanie01.cmd
@@ -1,6 +1,6 @@
+@echo off
 REM // 1. Zapoznać się z możliwościami konﬁguracji okna Wiersza polecenia.
 REM // (a) Zmień tytuł okna wiersza polecenia na Systemy operacyjne - laboratorium. 
 REM // (b) Zmień kolor tla wiersza polecenia na niebieski, a kolor czcionki na żółty.
 
-@echo off
 title "Systemy operacyjne - laboratorium" & color 16
