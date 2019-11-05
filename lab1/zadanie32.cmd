6f7b4be5dbecb2cee02911a6cdf324ffec0cf927
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-11-05 14:14:44 +0100

    Update zadanie32.cmd

--- lab1/zadanie32.cmd
+++ lab1/zadanie32.cmd
@@ -2,3 +2,6 @@
 REM 32. Napisać skrypt, który będzie podawał status wyjścia aplikacji uruchomionej przez skrypt.
 REM Skrypt ma zapytać się użytkownika o nazwę aplikacji, którą ma uruchomić.
 
+SET /p nazwa = Podaj nazwe aplikacji, ktora mam uruchomic: 
+CALL %nazwa%
+ECHO Aplikacja zakonczona z kodem %ERRORLEVEL%.
