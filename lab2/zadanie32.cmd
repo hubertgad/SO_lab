993e11dc822e6ecf7c4204accce32489a7d69be5
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-11-05 14:32:06 +0100

    Create zadanie32.cmd

---
+++ lab2/zadanie32.cmd
@@ -0,0 +1,6 @@
+@echo off
+REM 32. Dokonaj inspekcji zdarzeń logowania na kontach. 
+REM Do dziennika bezpieczeństwa należy zapisywać zdarzenia Sukces jak i Niepowodzenie.
+REM Wskazówka: użyj polecenia secpol.msc.
+
+secpol.msc
