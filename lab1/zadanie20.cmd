1eb3ecc3efb5d3c3f6a2451131d2741e08e416d3
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-11-05 08:20:20 +0100

    Update zadanie20.cmd

--- lab1/zadanie20.cmd
+++ lab1/zadanie20.cmd
@@ -3,5 +3,5 @@
 REM // 20. Dodać zadanie do harmonogramu zadań, polegające na uruchomieniu programu defragmentator dysku w wybranym dniu o wybranej godzinie
 
 net start "task scheduler"
-schtasks /create /tr defrag /sc ONCE /st 15:00 
+schtasks /create /tr defrag /tn nazwa /sc ONCE /st 15:00 
 pause
