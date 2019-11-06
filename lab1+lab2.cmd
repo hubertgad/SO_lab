24408b68d8b7609ed8801b8fdf054706b467c1fa
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-11-06 12:38:58 +0100

    Update lab1+lab2.cmd

--- lab1+lab2.cmd
+++ lab1+lab2.cmd
@@ -58,7 +58,7 @@ REM 17. Utworzyc plik wsadowy, który usunie wszystkie pliki o rozszerzeniu .bak
 REM podanego jako parametr. Skrypt powinien sprawdzac czy podano parametr. 
 REM Jesli go nie podano, to na ekranie powinna zostac wydrukowana poprawna składnia uzycia polecenia.
 IF [%1] NEQ [] (
-DEL %1\*.bak % DEL %1\*.tmp
+DEL %1\*.bak & DEL %1\*.tmp
 ) ELSE (
 ECHO Podaj sciezke do katalogu! )
 
